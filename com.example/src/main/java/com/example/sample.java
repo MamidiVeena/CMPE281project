1ce718dd5b32d0cb250ab9fdee9dba9870f3f826
KSK <KSK@DESKTOP-HGIU473.attlocal.net>
2021-03-22 19:41:41 -0700

    first commit

---
+++ com.example/src/main/java/com/example/sample.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import java.io.*; 
+
+public class sample {
+	public static void main(String[] args) {
+		System.out.println("Hello! This is sample project for CMPE 281 deployed on Google cloud");
+}
+}
